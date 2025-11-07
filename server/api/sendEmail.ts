f2f6283c64a5177e418dece59d082f2e2f1e6510
Soraia <sorilc@hotmail.com>
2025-11-07 20:29:55 +0100

    fix

--- server/api/sendEmail.ts
+++
@@ -1,5 +0,0 @@
-export default defineEventHandler(() => {
-  const { sendMail } = useNodeMailer()
-
-  return sendMail({ subject: 'Nuxt + nodemailer', text: 'Hello from nuxt-nodemailer!', to: 'john@doe.com' })
-})
